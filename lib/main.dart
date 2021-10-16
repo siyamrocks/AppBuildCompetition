0c4750e167563d0174e2e2ddf3ffa6c0827e6f71
alamshafil <36016500+alamshafil@users.noreply.github.com>
2021-10-15 22:40:26 -0400

    Login system

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Welcome/welcome_screen.dart';
-import 'package:flutter_auth/constants.dart';
+import 'login_page.dart';
+import 'home_page.dart';
+import 'register_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+    RegisterPage.tag: (context) => RegisterPage(),
+  };
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
-      theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
-      ),
-      home: WelcomeScreen(),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+      },
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MaterialApp(
+            title: 'Login',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.lightBlue,
+              fontFamily: 'Nunito',
+            ),
+            home: LoginPage(),
+            routes: routes,
+          )),
     );
   }
 }
