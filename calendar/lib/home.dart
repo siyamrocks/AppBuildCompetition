998d9ca7e4bbfc52bf776ea89d2634a8afa00bda
yngking <63524182+yngking@users.noreply.github.com>
2021-10-16 22:20:32 -0400

    Add files via upload

--- calendar/lib/home.dart
+++ calendar/lib/home.dart
@@ -1,16 +1,11 @@
 import 'package:calendar/calendar.dart';
-import 'package:calendar/splash.dart';
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Splash(),
+      home: Calendar(),
       title: "ESTech Calendar",
     );
   }
