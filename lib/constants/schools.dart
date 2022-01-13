2761764b9d9c3ccbd578ba205e9c48277a16f41b
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-01-12 19:35:57 -0500

    Add clubs data

--- lib/constants/schools.dart
+++ lib/constants/schools.dart
@@ -8,4 +8,12 @@ class SchoolData {
           "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_fa75uf4g098fe34vuc3vt4pfqk%40group.calendar.google.com&ctz=America%2FNew_York"
     }
   ];
+
+  static const List Clubs = [
+    {
+      "name": "grayson-high",
+      "url":
+          "https://docs.google.com/viewer?url=https%3A%2F%2Fwww.gcpsk12.org%2F%2Fcms%2Flib%2FGA02204486%2FCentricity%2FDomain%2F8265%2FDocuments%2F21-22%20Clubs.xlsx&embedded=true"
+    }
+  ];
 }
