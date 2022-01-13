3d25df42f867c77d9b8faa1e6b0b05694a330b53
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-01-12 20:19:21 -0500

    Get clubs school data ready

--- lib/constants/schools.dart
+++ lib/constants/schools.dart
@@ -104,6 +104,96 @@ class SchoolData {
       "name": "grayson-high",
       "url":
           "https://docs.google.com/viewer?url=https%3A%2F%2Fwww.gcpsk12.org%2F%2Fcms%2Flib%2FGA02204486%2FCentricity%2FDomain%2F8265%2FDocuments%2F21-22%20Clubs.xlsx&embedded=true"
+    },
+    {
+      "name": "archer-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=1fkclipb518g0ah3d9op82t3a8t2dhfh%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "berkmar-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=3fg32tfgbo9bdslco7veeosu5qbvk7es%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "brookwood-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=7k956hd1rfilv6s5t6p0dhnu306bjbrg%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "buford-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=o2cgv03ojb2rukapdldc6r92l4d2svll%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "central-gwinnett-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=rms94bm349479p55rgp6t8uho4fjrmt8%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "collins-hill-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_pq2c5cj8cl1d8tas8g0v6u7l48%40group.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "dacula-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=1kpta45iktb9fga0pb64t4190k56lls3%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "discovery-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=j64gv776nmppsufbpdrqmcn6o23ijt1f%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "duluth-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=0d2i2p8c0q6b2vj7ikif5vrd4ei0nu0b%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "meadowcreek-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_ehv07eicq9f6ia8uu8jc38a9us%40group.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "lanier-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_c2lmc6boi5t2v3vidtjcbm6uao%40group.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "mill-creek-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=q2is42aej6adpatsb1s9hk73d784mo2i%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "norcross-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=l8vogjss89qnmuu9ooos6gds2q3ekfll%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "north-gwinnett-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_vf4bvuit32s1fn0m99i4h2rmc4%40group.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "parkview-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=kjfv8jac8hbl17sc7bpil8lljofcp81d%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "peachtree-ridge-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=gwinnett.k12.ga.us_kdtt7msae5tpktmnpjhtadqvc8%40group.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "shiloh-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=bvknu9csfv3jhmuuimb64c4iqrsqdlia%40import.calendar.google.com&ctz=America%2FNew_York"
+    },
+    {
+      "name": "south-gwinnett-high",
+      "url":
+          "https://calendar.google.com/calendar/embed?src=2ke5nsuhrmnvmcrd6j7nnj56b816vjhn%40import.calendar.google.com&ctz=America%2FNew_York"
     }
   ];
 }
