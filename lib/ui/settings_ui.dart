30e65a681a5eb2b6fe4605f54759b3be0089ece3
Benito Karkada <benito1karkada@gmail.com>
2022-01-19 08:25:03 -0500

    settingsquestionmark

--- lib/ui/settings_ui.dart
+++ lib/ui/settings_ui.dart
@@ -37,140 +37,142 @@ class SettingsUI extends StatelessWidget {
       MenuOptionsModel(
           key: "dark", value: labels.settings.dark, icon: Icons.brightness_3)
     ];
-    return ListView(
-      padding: EdgeInsets.all(15.0),
-      children: <Widget>[
-        Text(
-          "Theme",
-          style: TextStyle(
-            fontSize: 24.0,
-            color: Colors.amber,
+    Expanded(
+      child: ListView(
+        padding: EdgeInsets.all(15.0),
+        children: <Widget>[
+          Text(
+            "Theme",
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.amber,
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: SegmentedSelector(
-            selectedOption: Provider.of<ThemeProvider>(context).getTheme,
-            menuOptions: themeOptions,
-            onValueChanged: (value) {
-              Provider.of<ThemeProvider>(context, listen: false)
-                  .updateTheme(value);
-            },
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: SegmentedSelector(
+              selectedOption: Provider.of<ThemeProvider>(context).getTheme,
+              menuOptions: themeOptions,
+              onValueChanged: (value) {
+                Provider.of<ThemeProvider>(context, listen: false)
+                    .updateTheme(value);
+              },
+            ),
           ),
-        ),
-        Text(
-          "System",
-          style: TextStyle(
-            fontSize: 24.0,
-            color: Colors.amber,
+          Text(
+            "System",
+            style: TextStyle(
+              fontSize: 24.0,
+              color: Colors.amber,
+            ),
           ),
-        ),
-        Card(
-          elevation: 4.0,
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.translate),
-                title: Text(
-                  labels.settings.language,
-                  style: TextStyle(fontSize: 15),
-                ),
-                trailing: Container(
-                  child: DropdownPicker(
-                    menuOptions: Globals.languageOptions,
-                    selectedOption:
-                        Provider.of<LanguageProvider>(context).currentLanguage,
-                    onChanged: (value) {
-                      Provider.of<LanguageProvider>(context, listen: false)
-                          .updateLanguage(value);
-                    },
+          Card(
+            elevation: 4.0,
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.translate),
+                  title: Text(
+                    labels.settings.language,
+                    style: TextStyle(fontSize: 15),
                   ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text(labels.settings.updateProfile),
-                trailing: Container(
-                  width: 90,
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      Navigator.of(context).pushNamed('/update-profile');
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.amber)),
-                    child: Text(
-                      labels.settings.updateProfile,
-                      textAlign: TextAlign.center,
+                  trailing: Container(
+                    child: DropdownPicker(
+                      menuOptions: Globals.languageOptions,
+                      selectedOption: Provider.of<LanguageProvider>(context)
+                          .currentLanguage,
+                      onChanged: (value) {
+                        Provider.of<LanguageProvider>(context, listen: false)
+                            .updateLanguage(value);
+                      },
                     ),
                   ),
                 ),
-              ),
-              ListTile(
-                leading: Icon(Icons.bug_report),
-                title: Text("Report Bug"),
-                trailing: Container(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      Navigator.of(context).pushNamed('/bug-report');
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.amber)),
-                    child: Text(
-                      "Report Bug",
-                      textAlign: TextAlign.center,
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text(labels.settings.updateProfile),
+                  trailing: Container(
+                    width: 90,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Navigator.of(context).pushNamed('/update-profile');
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.amber)),
+                      child: Text(
+                        labels.settings.updateProfile,
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings_applications),
-                title: Text("License"),
-                trailing: Container(
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      showLicensePage(context: context);
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.amber)),
-                    child: Text(
-                      "License",
-                      textAlign: TextAlign.center,
+                ListTile(
+                  leading: Icon(Icons.bug_report),
+                  title: Text("Report Bug"),
+                  trailing: Container(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        Navigator.of(context).pushNamed('/bug-report');
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.amber)),
+                      child: Text(
+                        "Report Bug",
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              ListTile(
-                leading: Icon(Icons.switch_account),
-                title: Text(labels.settings.signOut),
-                trailing: Container(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Provider.of<StudentVueProvider>(context, listen: false)
-                          .resetData();
-                      _sharedPrefsHelper.setPassword("");
-                      AuthService _auth = AuthService();
-                      _auth.signOut();
-                    },
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.amber)),
-                    child: Text(
-                      labels.settings.signOut,
-                      textAlign: TextAlign.center,
+                ListTile(
+                  leading: Icon(Icons.settings_applications),
+                  title: Text("License"),
+                  trailing: Container(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        showLicensePage(context: context);
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.amber)),
+                      child: Text(
+                        "License",
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                ListTile(
+                  leading: Icon(Icons.switch_account),
+                  title: Text(labels.settings.signOut),
+                  trailing: Container(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Provider.of<StudentVueProvider>(context, listen: false)
+                            .resetData();
+                        _sharedPrefsHelper.setPassword("");
+                        AuthService _auth = AuthService();
+                        _auth.signOut();
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all<Color>(Colors.amber)),
+                      child: Text(
+                        labels.settings.signOut,
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.all(15.0),
-        ),
-      ],
+          Padding(
+            padding: EdgeInsets.all(15.0),
+          ),
+        ],
+      ),
     );
   }
 }
