9a27783b74515248e50b78e9698cea89c619cfd0
alamshafil <36016500+alamshafil@users.noreply.github.com>
2022-01-30 13:51:14 -0500

    Fix social

--- lib/ui/social.dart
+++ lib/ui/social.dart
@@ -1,7 +1,5 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_starter/constants/schools.dart';
-import 'package:flutter_starter/models/models.dart';
-import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class Social extends StatefulWidget {
@@ -12,8 +10,16 @@ class Social extends StatefulWidget {
 class _SocialState extends State<Social> {
   @override
   Widget build(BuildContext context) {
-    String url = "https://twitter.com/GwinnettSchools?ref_src=twsrc%5Etfw";
     return WebView(
-        javascriptMode: JavascriptMode.unrestricted, initialUrl: url);
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: "about:blank",
+      onWebViewCreated: (controller) {
+        controller.loadUrl(Uri.dataFromString(
+                '<a class="twitter-timeline" href="https://twitter.com/GwinnettSchools?ref_src=twsrc%5Etfw">Tweets by GwinnettSchools</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
+                mimeType: 'text/html',
+                encoding: utf8)
+            .toString());
+      },
+    );
   }
 }
