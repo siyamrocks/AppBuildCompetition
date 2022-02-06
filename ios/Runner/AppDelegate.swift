8f9286320d1e225b362a556da9858b42c58dcc09
Benito Karkada <benito1karkada@gmail.com>
2022-02-06 18:17:41 -0500

    fixedagain

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Flutter
 import Firebase
-import OneSignal
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,8 +9,6 @@ import OneSignal
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    OneSignal.initWithLaunchOptions(launchOptions)
-    OneSignal.setAppId("2244a21f-4b0f-4628-8094-2396c8718394")
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
